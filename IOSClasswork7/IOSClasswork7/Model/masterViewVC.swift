d0e2e90260b5490138d332a2a8a50c433ca13be9
ali3dashti <aadashti73@gmail.com>
2020-07-04 21:05:32 +0300

    تم حل الكلاس ورك ٢

--- IOSClasswork7/IOSClasswork7/Model/masterViewVC.swift
+++ IOSClasswork7/IOSClasswork7/Model/masterViewVC.swift
@@ -40,7 +40,7 @@ class masterViewVC: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
-         performSegue(withIdentifier: "next", sender: nil)
+        performSegue(withIdentifier: "next", sender: indexPath.row)
     }
 
     /*
@@ -78,14 +78,20 @@ class masterViewVC: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "next" {
+            let index = sender as! Int
+            let vc = segue.destination as! MovieDetailsViewController
+            vc.movieData = MoviesData[index]
+            
+        }
     }
-    */
+    
 
 }
